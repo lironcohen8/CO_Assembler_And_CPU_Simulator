2398206882f591c0da0d2a62291164f521a9e526
Ron Federman <ronfederman@mail.tau.ac.il>
2021-11-03 13:48:26 +0200

    First asembler commit from library

---
+++ src/asembler/asembler.h
@@ -0,0 +1,41 @@
+#include <stdint.h>
+#include "reg.h"
+
+#define MAX_LINE_LENGTH (500)
+#define MAX_LABEL_LENGTH (50)
+
+typedef struct {
+    uint8_t opcode :  8;
+    reg_e rd       :  4;
+    reg_e rs       :  4;
+    reg_e rt       :  4;
+    reg_e rm       :  4;
+    uint16_t imm1  : 12;
+    uint16_t imm2  : 12;
+} asm_line_t;
+
+typedef enum {
+    ADD,
+    SUB,
+    MAC,
+    AND,
+    OR,
+    XOR,
+    SLL,
+    SRA,
+    SRL,
+    BEQ,
+    BNE,
+    BLT,
+    BGT,
+    BLE,
+    BGE,
+    JAL,
+    LW,
+    SW,
+    RETI,
+    IN,
+    OUT,
+    HALT
+} opcode_e;
+
---
+++ src/asembler/reg.h
@@ -0,0 +1,18 @@
+typedef enum {
+    $ZERO,
+    $IMM1,
+    $IMM2,
+    $V0,
+    $A0,
+    $A1,
+    $A2,
+    $T0,
+    $T1,
+    $T2,
+    $S0,
+    $S1,
+    $S2,
+    $GP,
+    $SP,
+    $RA
+} reg_e;
