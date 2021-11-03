a958e86a57ac42408871d6c1441850a9ef7226a4
RonFed <ronfederman@mail.tau.ac.il>
2021-11-03 22:17:01 +0200

    Lookup of labels in asm file

--- src/assembler/assembler.c
+++ src/assembler/assembler.c
@@ -1,4 +1,4 @@
-#include "asembler.h"
+#include "assembler.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,25 +9,26 @@ char* strcpy(char* destination, const char* source);
 
 int main(int argc, char const *argv[])
 {
-    char* filename = argv[1], buffer[MAX_LINE_LENGTH];
-    FILE* asm_program = fopen(filename);
+    char const* filename = argv[1];
+    char buffer[MAX_LINE_LENGTH];
+    FILE* asm_program = fopen(filename, "r");
     int line_counter = -1;
     int label_counter = 0, curr_arr_len = BASE_ARR_SIZE;
-    char* tmp_label;
-    label_t* labels_list = calloc(BASE_ARR_SIZE, sizeof(label_t));
-
+    label_t* labels_list = (label_t*)calloc(BASE_ARR_SIZE, sizeof(label_t));
+    char* tmp_label_str;
 
     while (fgets(buffer,MAX_LINE_LENGTH, asm_program) != NULL) {
         line_counter++;
         for (int i = 0 ; i < MAX_LABEL_LENGTH + 1; i++) {
             if (buffer[i] == ':') {
                 buffer[i] = '\0';
-                strcpy(tmp_label,buffer);
-                label_t tmp_label = {.label = tmp_label, .op_index = line_counter--}; /* label lines don't count as instruction lines */
+                tmp_label_str = (char *) malloc(MAX_LABEL_LENGTH * sizeof(char));
+                strcpy(tmp_label_str, buffer);
+                label_t tmp_label = {.label = tmp_label_str, .op_index = line_counter--}; /* label lines don't count as instruction lines */
                 if (label_counter == curr_arr_len)
                 {
                     curr_arr_len *= ARR_SIZE_MULTIPLY;
-                    labels_list = realloc(labels_list, curr_arr_len);
+                    labels_list = (label_t*)realloc(labels_list, curr_arr_len * sizeof(label_t));
                 }
                 
                 labels_list[label_counter++] = tmp_label;
@@ -36,5 +37,6 @@ int main(int argc, char const *argv[])
         }
     }
 
-    labels_list = realloc(labels_list, label_counter);
+    labels_list = (label_t*)realloc(labels_list, label_counter * sizeof(label_t));
+    printf("hi");
 }
--- src/assembler/assembler.h
+++ src/assembler/assembler.h
@@ -1,18 +1,8 @@
 #include <stdint.h>
-#include "reg.h"
+#include "../reg.h"
 
 #define MAX_LINE_LENGTH (500)
 #define MAX_LABEL_LENGTH (50)
-
-typedef struct {
-    opcode_e opcode:  8;
-    reg_e rd       :  4;
-    reg_e rs       :  4;
-    reg_e rt       :  4;
-    reg_e rm       :  4;
-    uint16_t imm1  : 12;
-    uint16_t imm2  : 12;
-} asm_line_t;
 
 typedef enum {
     ADD,
@@ -40,6 +30,16 @@ typedef enum {
 } opcode_e;
 
 typedef struct {
-    char label[MAX_LABEL_LENGTH];
+    opcode_e opcode:  8;
+    reg_e rd       :  4;
+    reg_e rs       :  4;
+    reg_e rt       :  4;
+    reg_e rm       :  4;
+    uint16_t imm1  : 12;
+    uint16_t imm2  : 12;
+} asm_line_t;
+
+typedef struct {
+    char * label;
     int op_index;
 } label_t;
