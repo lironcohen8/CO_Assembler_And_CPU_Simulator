eea0fe7b249aeef5f6f981117f1d3fe2cfd79157
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-08 18:27:22 +0200

    Update fib.asm

--- src/fib.asm
+++ src/fib.asm
@@ -1,4 +1,4 @@
-out $zero, $zero, $imm2, $imm1, 1, 2		# enable irq2
+	out $zero, $zero, $imm2, $imm1, 1, 2		# enable irq2
 	sll $sp, $imm1, $imm2, $zero, 1, 11		# set $sp = 1 << 11 = 2048
 	out $zero, $imm1, $zero, $imm2, 6, L3		# set irqhandler as L3
 	lw $a0, $zero, $imm2, $zero, 0, 64		# get x from address 64
