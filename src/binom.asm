5531d40a70ce8906eb446b3bd2e11a2da1758ec2
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-11-21 21:41:31 +0200

    Update binom.asm

--- src/binom.asm
+++ src/binom.asm
@@ -5,7 +5,8 @@ main:
 	sw $zero, $zero, $imm2, $v0, 0, 102		# store binom(n,k) in 0x102
 	halt $zero, $zero, $zero, $zero, 0, 0	# end
 binom:
-	add $sp, $sp, $imm2, $zero, 0, -3		# adjust stack for 3 items
+	add $sp, $sp, $imm2, $zero, 0, -4		# adjust stack for 3 items
+	sw $s0, $sp, $imm2, $zero, 0, 3			# save $s0 in $sp+3
 	sw $a0, $sp, $imm2, $zero, 0, 2			# save $a0 in $sp+2
 	sw $a1, $sp, $imm2, $zero, 0, 1			# save $a1 in $sp+1
 	sw $ra, $sp, $imm2, $zero, 0, 0			# save return address in $sp+0
@@ -17,12 +18,14 @@ infunc:
 	jal $ra, $zero, $zero, $imm2, 0, binom	# $v0 = binom(n-1,k-1)
 	add $s0, $v0, $zero, $zero, 0, 0		# $s0 = binom(n-1,k-1)
 	add $a1, $a1, $zero, $imm2, 0, 1		# $a1 = k
+	jal $ra, $zero, $zero, $imm2, 0, binom	# $v0 = binom(n-1,k)
 	add $v0, $v0, $s0, $zero, 0, 0			# $v0 = binom(n-1,k-1) + binom(n-1,k)
-	beq $zero, $zero, $zero, $zero, 0, ret	# jump to ret
+	beq $zero, $zero, $zero, $imm2, 0, ret	# jump to ret
 base:
 	add $v0, $zero, $zero, $imm1, 0, 1		# $v0 = 1
 ret:
-	add $sp, $sp, $imm2, $zero, 0, -3		# adjust stack for 3 items
+	add $sp, $sp, $imm2, $zero, 0, -4		# adjust stack for 3 items
+	lw $s0, $sp, $imm2, $zero, 0, 3			# load $s0 from $sp+3
 	lw $a0, $sp, $imm2, $zero, 0, 2			# load $a0 from $sp+2
 	lw $a1, $sp, $imm2, $zero, 0, 1			# load $a1 from $sp+1
 	lw $ra, $sp, $imm2, $zero, 0, 0			# load return address from $sp+0
