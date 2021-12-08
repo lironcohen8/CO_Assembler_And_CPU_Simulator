b81ee06d52d81c089c2952606954fae1c1b9bada
RonFed <ronfederman@mail.tau.ac.il>
2021-12-08 20:31:05 +0200

    Testing binom.asm

--- src/binom.asm
+++ src/binom.asm
@@ -1,11 +1,14 @@
+.word 100 1
+.word 101 1
 main:
+	sll $sp, $imm1, $imm2, $zero, 1, 11		# set $sp = 1 << 11 = 2048
 	lw $a0, $imm2, $zero, $zero, 0, 100		# $a0 = n
 	lw $a1, $imm2, $zero, $zero, 0, 101		# $a1 = k
 	jal $ra, $zero, $zero, $imm2, 0, binom	# $v0 = binom(n,k)
 	sw $zero, $zero, $imm2, $v0, 0, 102		# store binom(n,k) in 0x102
 	halt $zero, $zero, $zero, $zero, 0, 0	# end
 binom:
-	add $sp, $sp, $imm2, $zero, 0, -4		# adjust stack for 3 items
+	add $sp, $sp, $imm2, $zero, 0, -4		# adjust stack for 4 items
 	sw $s0, $sp, $imm2, $zero, 0, 3			# save $s0 in $sp+3
 	sw $a0, $sp, $imm2, $zero, 0, 2			# save $a0 in $sp+2
 	sw $a1, $sp, $imm2, $zero, 0, 1			# save $a1 in $sp+1
@@ -24,9 +27,9 @@ infunc:
 base:
 	add $v0, $zero, $zero, $imm1, 0, 1		# $v0 = 1
 ret:
-	add $sp, $sp, $imm2, $zero, 0, -4		# adjust stack for 3 items
 	lw $s0, $sp, $imm2, $zero, 0, 3			# load $s0 from $sp+3
 	lw $a0, $sp, $imm2, $zero, 0, 2			# load $a0 from $sp+2
 	lw $a1, $sp, $imm2, $zero, 0, 1			# load $a1 from $sp+1
 	lw $ra, $sp, $imm2, $zero, 0, 0			# load return address from $sp+0
+	add $sp, $sp, $imm2, $zero, 0, 4		# adjust stack for 4 items
 	beq $zero, $zero, $zero, $ra, 0, 0		# return
