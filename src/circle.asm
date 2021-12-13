a61481e8fcd711d90ce151b65c0d10344c600df8
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-12-13 15:04:43 +0200

    circle checks

--- src/circle.asm
+++ src/circle.asm
@@ -1,12 +1,12 @@
 .word 0x100 10
 main:
-	lw $s0, $zero, $imm2, $zero, 0, 100		# $s0 = R from address 0x100
+	lw $s0, $zero, $imm2, $zero, 0, 0x100		# $s0 = R from address 0x100
 	mac $s0, $s0, $s0, $zero, 0, 0			# $s0 = R^2
 	add $t0, $zero, $zero, $zero, 0, 0		# $t0 = index = 0
 	add $s1, $zero, $zero, $imm1, 65536, 0	# $s1 = 65536
 	add $s2, $zero, $zero, $imm1, -256, 0	# $s2 = -256
 loop:
-	bge $zero, $t0, $s1, $imm2, 0, end		# if index >= 65536 jump to end
+	bgt $zero, $t0, $s1, $imm2, 0, end		# if index >= 65536 jump to end
 	sra $t1, $t0, $imm1, $zero, 16, 0		# $t1 = $t0 / 256 = i
 	mac $t1, $t1, $t1, $zero, 0, 0			# $t1 = $t1 * $t1 = i^2
 	mac $t2, $s2, $t1, $t0, 0, 0			# $t2 = -256*(index/256)+index = index%256 = j
