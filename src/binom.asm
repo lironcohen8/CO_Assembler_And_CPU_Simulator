b4175faf645296e515fb4016c8e28cd04da21848
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-12-30 08:22:12 +0200

    updated labels names

--- src/binom.asm
+++ src/binom.asm
@@ -1,35 +1,35 @@
 .word 0x100 10
 .word 0x101 4
-main:
+MAIN:
 	sll $sp, $imm1, $imm2, $zero, 1, 11		# set $sp = 1 << 11 = 2048
 	lw $a0, $imm2, $zero, $zero, 0, 0x100	# $a0 = n
 	lw $a1, $imm2, $zero, $zero, 0, 0x101	# $a1 = k
-	jal $ra, $zero, $zero, $imm2, 0, binom	# $v0 = binom(n,k)
-	sw $v0, $zero, $imm2, $zero, 0, 0x102	# store binom(n,k) in 0x102
+	jal $ra, $zero, $zero, $imm2, 0, BINOM	# $v0 = BINOM(n,k)
+	sw $v0, $zero, $imm2, $zero, 0, 0x102	# store BINOM(n,k) in 0x102
 	halt $zero, $zero, $zero, $zero, 0, 0	# end
-binom:
+BINOM:
 	add $sp, $sp, $imm2, $zero, 0, -4		# adjust stack for 4 items
 	sw $s0, $sp, $imm2, $zero, 0, 3			# save $s0 in $sp+3
 	sw $a0, $sp, $imm2, $zero, 0, 2			# save $a0 in $sp+2
 	sw $a1, $sp, $imm2, $zero, 0, 1			# save $a1 in $sp+1
-	sw $ra, $sp, $imm2, $zero, 0, 0			# save return address in $sp+0
-infunc:
-	beq $zero, $a1, $zero, $imm2, 0, base	# if k == 0 jump to base
-	beq $zero, $a0, $a1, $imm2, 0, base		# if n == k jump to base
+	sw $ra, $sp, $imm2, $zero, 0, 0			# save RETurn address in $sp+0
+INFUNC:
+	beq $zero, $a1, $zero, $imm2, 0, BASE	# if k == 0 jump to BASE
+	beq $zero, $a0, $a1, $imm2, 0, BASE		# if n == k jump to BASE
 	add $a0, $a0, $zero, $imm2, 0, -1		# $a0 = n - 1
 	add $a1, $a1, $zero, $imm2, 0, -1		# $a1 = k - 1
-	jal $ra, $zero, $zero, $imm2, 0, binom	# $v0 = binom(n-1,k-1)
-	add $s0, $v0, $zero, $zero, 0, 0		# $s0 = binom(n-1,k-1)
+	jal $ra, $zero, $zero, $imm2, 0, BINOM	# $v0 = BINOM(n-1,k-1)
+	add $s0, $v0, $zero, $zero, 0, 0		# $s0 = BINOM(n-1,k-1)
 	add $a1, $a1, $zero, $imm2, 0, 1		# $a1 = k
-	jal $ra, $zero, $zero, $imm2, 0, binom	# $v0 = binom(n-1,k)
-	add $v0, $v0, $s0, $zero, 0, 0			# $v0 = binom(n-1,k-1) + binom(n-1,k)
-	beq $zero, $zero, $zero, $imm2, 0, ret	# jump to ret
-base:
+	jal $ra, $zero, $zero, $imm2, 0, BINOM	# $v0 = BINOM(n-1,k)
+	add $v0, $v0, $s0, $zero, 0, 0			# $v0 = BINOM(n-1,k-1) + BINOM(n-1,k)
+	beq $zero, $zero, $zero, $imm2, 0, RET	# jump to RET
+BASE:
 	add $v0, $zero, $zero, $imm2, 0, 1		# $v0 = 1
-ret:
+RET:
 	lw $s0, $sp, $imm2, $zero, 0, 3			# load $s0 from $sp+3
 	lw $a0, $sp, $imm2, $zero, 0, 2			# load $a0 from $sp+2
 	lw $a1, $sp, $imm2, $zero, 0, 1			# load $a1 from $sp+1
-	lw $ra, $sp, $imm2, $zero, 0, 0			# load return address from $sp+0
+	lw $ra, $sp, $imm2, $zero, 0, 0			# load RETurn address from $sp+0
 	add $sp, $sp, $imm2, $zero, 0, 4		# adjust stack for 4 items
-	beq $zero, $zero, $zero, $ra, 0, 0		# return
+	beq $zero, $zero, $zero, $ra, 0, 0		# RETurn
--- src/circle.asm
+++ src/circle.asm
@@ -1,13 +1,13 @@
 .word 0x100 100
-main:
+MAIN:
 	lw $s0, $zero, $imm2, $zero, 0, 0x100	# $s0 = R from address 0x100
 	mac $s0, $s0, $s0, $zero, 0, 0			# $s0 = R^2
 	add $t0, $zero, $zero, $zero, 0, 0		# $t0 = index = 0
 	add $s2, $zero, $zero, $imm1, 256, 0	# $s2 = 256
 	mac $s1, $s2, $s2, $zero, 0, 0			# $s1 = monitor size 65536
 	add $s2, $zero, $zero, $imm1, 255, 0	# $s2 = 255
-loop:
-	bge $zero, $t0, $s1, $imm2, 0, end		# if index >= 65536 jump to end
+LOOP:
+	bge $zero, $t0, $s1, $imm2, 0, END		# if index >= 65536 jump to END
 	sra $t1, $t0, $imm1, $zero, 8, 0		# $t1 = index / 256 = i
 	and $t2, $t0, $s2, $s2, 0, 0			# $t2 = index & 255 = j
 	sub $t1, $t1, $imm1, $zero, 128, 0		# $t1 = i - 128
@@ -15,12 +15,12 @@ loop:
 	mac $t1, $t1, $t1, $zero, 0, 0			# $t1 = $t1 * $t1 = (i-128)^2
 	mac $t2, $t2, $t2, $zero, 0, 0			# $t2 = $t2 * $t2 = (j-128)^2
 	add $t1, $t1, $t2, $zero, 0, 0			# $t1 = (i-128)^2 + (j-128)^2
-	bgt $zero, $t1, $s0, $imm2, 0, inc		# if out of circle jump to inc
+	bgt $zero, $t1, $s0, $imm2, 0, INC		# if out of circle jump to INC
 	out $zero, $zero, $imm2, $t0, 0, 20 	# if in circle set index as address
 	out $zero, $zero, $imm2, $imm1, 255, 21	# set pixel color to white
 	out $zero, $zero, $imm2, $imm1, 1, 22	# draw pixel
-inc:
+INC:
 	add $t0, $t0, $imm1, $zero, 1, 0		# index++
-	beq $zero, $zero, $zero, $imm2, 0, loop	# jump to loop
-end:
+	beq $zero, $zero, $zero, $imm2, 0, LOOP	# jump to LOOP
+END:
 	halt $zero, $zero, $zero, $zero, 0, 0	# halt
